ef46d6055efa92f64b70ad4af393ed3794477ceb
TwilCynder <twilcynder@gmail.com>
2024-02-01 17:52:44 +0100

    some code  for the discord occitour bot

--- OcciTour/discord/functions.js
+++ OcciTour/discord/functions.js
@@ -1,4 +1,5 @@
 import { Client } from "discord.js";
+import { join } from "path"
 
 /**
  * 
@@ -23,4 +24,31 @@ export function processList(listText){
 export async function sendMessage(client, channelID, message){
     let channel = await client.channels.fetch(channelID);
     channel.send(message);
+}
+
+function loadToken(path){
+    path = path ?? import.meta.url;
+    let token;
+    try {
+        let res = fs.readFileSync(join(path, "secrets.json"));
+        res = JSON.parse(res);
+        if (!res && !res.token) throw "Could not find a token property in secrets.json";
+        token = res.token;
+    } catch (err) {
+        if (err instanceof SyntaxError){
+            console.error("secrets.json coudn't be parsed, its syntax is incorrect : ", err);
+        } else if (typeof err == "string") {
+            console.error("Error reading token in secrets.json :", err);
+        } else {
+            console.error("Cannot read secrets.json. If it doesn't exist, please create a secrets.json file containing  {\"token\" : BOT_TOKEN} ");
+        }
+        process.exit(1);
+    }
+}
+
+async function initBot(secretsPath){
+    let token = loadToken(secretsPath);
+    let client = new Client({intents :  ["Guilds", "GuildMessages", "MessageContent", "GuildMessageReactions"]})
+    await client.login(token);
+    return client;
 }
