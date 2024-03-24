abb819346934d88c7adaabdd1aa68970d539d792
TwilCynder <twilcynder@gmail.com>
2024-03-24 21:17:32 +0100

    fix preview cors issue (now script is inlined)

--- OcciTour/preview/main.js
+++ OcciTour/preview/main.js
@@ -6,18 +6,22 @@ if (process.argv.length < 3){
     process.exit(1);
 }
 
-let [template, data] = await Promise.all([
+let [template, data, script] = await Promise.all([
     fs.readFile(new URL("./template/index.html", import.meta.url))
         .then(buf => buf.toString())
         .catch(err => {console.error("Couldn't open template :", err) ; process.exit(2)}),
     fs.readFile(process.argv[2])
         .then(buf => JSON.parse(buf.toString()))
         .catch(err => {console.error("Couldn't open data :", err) ; process.exit(2)}),
+    fs.readFile(new URL("./page/index.js", import.meta.url))
+        .then(buf => buf.toString())
+        .catch(err => {console.error("Couldn't open page's script (should be at Occitour/preview/page/index.js) : ", err) ; process.exit(3)})
 ])
 
 let content = generateTable(data);
 
-let result = template.replace("<!--TEMPLATE-->", content);
+let result = template.replace("<!--TEMPLATE-->", content).replace("<!--JS-->", '<script type = "module">' + script + "</script>");
+
 
 let outputFilename = process.argv[3] ?? new URL("./page/index.html", import.meta.url);
 fs.writeFile(outputFilename, result);
